f263ee719cfcee022757ae261386ba91523ff5d0
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2024-03-23 20:46:32 +0500

    first commit

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+import express from "express";
+
+const app = express();
+// app.use()
+
+
+
+const jokes= [
+  {
+    setup: "Why did the JavaScript developer go broke?",
+    punchline: "Because he used up all his cache!",
+  },
+  {
+    setup: "Why did the JavaScript developer quit his job?",
+    punchline: "Because he didn't get arrays!",
+  },
+  {
+    setup: "Why was the JavaScript developer sad?",
+    punchline: "Because he didn't know how to `null` his emotions.",
+  },
+  {
+    setup: "Why did the JavaScript function break up with the HTML element?",
+    punchline: "It just wasn't his type!",
+  },
+  {
+    setup: "Why was the JavaScript developer always lonely?",
+    punchline: "Because he didn't know how to `fetch` a date!",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("server is ready");
+});
+
+app.get("/api/jokes", (req, res) => {
+  res.send({ alljokes: jokes });
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ frontend/src/App.tsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+
+import "./App.css";
+import axios from "axios";
+
+export interface JOKESTYPE {
+  setup: string;
+  punchline: string;
+}
+
+interface ALLJOKETYPE {
+  alljokes: JOKESTYPE[];
+}
+function App() {
+  const [jokesData, setJokesData] = useState<ALLJOKETYPE>({ alljokes: [] });
+  const [error, setError] = useState<string | null>(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get<ALLJOKETYPE>("/api/jokes");
+
+        setJokesData(response.data ?? { alljokes: [] });
+      } catch (error: string | null) {
+        setError(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <p>ALL JOKES {jokesData.alljokes.length}</p>
+      {jokesData.alljokes.length > 0 &&
+        jokesData.alljokes.map((joke: JOKESTYPE, index: number) => (
+          <div key={index}>
+            <p style={{ backgroundColor: "red", margin: "1px" }}>
+              {joke.setup}
+            </p>
+          </div>
+        ))}
+      {error && <p>Error: {error}</p>}
+    </div>
+  );
+}
+
+export default App;
