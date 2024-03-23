429efe6edd3ed9e8e9756ac65b65ed2b074c3a31
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2024-03-24 03:14:42 +0500

    integrate mongodb

--- taskapp/backend/src/server.js
+++ taskapp/backend/src/server.js
@@ -1,13 +1,12 @@
-import mongoose from 'mongoose';
-import connectDB from './db';
+import mongoose from "mongoose";
+import connectDB from "./db";
 
 connectDB()
-.then(() => {
+  .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-    })
-})
-.catch((err) => {
+      console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
     console.log("MONGO db connection failed !!! ", err);
-})
-
+  });
