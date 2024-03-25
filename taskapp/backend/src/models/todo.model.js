401ac57bd76b35835cb1037e92836cec132a3668
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2024-03-25 21:17:15 +0500

    integrate frontend

---
+++ taskapp/backend/src/models/todo.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+const todoSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  complete: {
+    type: Boolean,
+    default: false,
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+export const Todo = mongoose.model("Todo", todoSchema);
