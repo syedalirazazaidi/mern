4059a65dc5e7f17f9f517b0c343e52f2a16cc14a
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2024-04-23 21:11:03 +0500

    3 sum using for loop

--- pract.js
+++ pract.js
@@ -32,17 +32,38 @@
 // console.log(newarr);
 
 // sort an array using 2 pointer algorithms
-var arraysort = [3, 45, 1, 190,0];
-var left = 0;
-var right = arraysort.length - 1;
-while (left < right) {
-  if (arraysort[left] < arraysort[right]) {
-    [arraysort[left], arraysort[right]] = [arraysort[right], arraysort[left]];
-  }
-  left++
-  if(left>=right){
-    left=0;
-    right--
-  }
-}
-console.log(arraysort);
+// var arraysort = [3, 45, 1, 190, 0];
+// var left = 0;
+// var right = arraysort.length - 1;
+// while (left < right) {
+//   if (arraysort[left] < arraysort[right]) {
+//     [arraysort[left], arraysort[right]] = [arraysort[right], arraysort[left]];
+//   }
+//   left++;
+//   if (left >= right) {
+//     left = 0;
+//     right--;
+//   }
+// }
+// console.log(arraysort);
+//using 2 pointer algorithms Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+// Notice that the solution set must not contain duplicate triplets.
+
+
+// using for loop
+// var nums = [0, 1, 1];
+// var newarr = [];
+// for (let i = 0; i < nums.length; i++) {
+//   for (let j = i + 1; j < nums.length; j++) {
+//     for (let k = j + 1; k < nums.length; k++) {
+//       var sum = nums[i] + nums[j] + nums[k];
+//       if (sum === 0) {
+//         newarr.push([nums[i], nums[j], nums[k]]);
+//       }
+//     }
+//   }
+// }
+// console.log(newarr);
+
+// using 2 pointer
